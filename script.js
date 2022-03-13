d4912c5edb2dc0d5558ce2e2e65857f068c6a4c8
Shoenemann <85433055+Shoenemann@users.noreply.github.com>
2022-03-13 05:48:14 +0100

    Create grid with JS

---
+++ script.js
@@ -0,0 +1,36 @@
+function getContainerGrid () {
+    return document.querySelector('.containerGrid')
+}
+
+function createRow(position,container) {
+    let row = document.createElement('div')
+    row.classList.add('rowGrid')
+    row.id = `row${position}`
+    container.appendChild(row)
+    return row
+}
+
+function createCell(x,y,containerRow) {
+    let cell = document.createElement('div')
+    cell.classList.add('cellGrid')
+    cell.id = `row${x}col${y}`
+    containerRow.appendChild(cell)
+    return cell
+}
+
+function createGrid(numRows) {
+    const div = getContainerGrid()
+    const rows = new Array(numRows)
+    const cells = new Array(numRows)
+    for (let i = 0; i<numRows; i++) {
+        cells [i] = new Array(numRows)
+        rows[i] = createRow(i,div)
+    }
+    for (let x = 0; x<numRows; x++) {
+        for (let y = 0; y<numRows; y++) {
+            cells[x][y] = createCell(x,y,rows[x])
+        }
+    }
+}
+
+createGrid(16)
